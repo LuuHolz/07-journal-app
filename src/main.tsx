53f109bf4267bcd192b358cde49b791d07ef6084
Luciana <luciana.holzmann@gmail.com>
2024-07-25 16:52:14 +0200

    [CHORE] Configuración de Redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { JournalApp } from './pages/JournalApp'
-import './styles.scss'
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { JournalApp } from "./pages/JournalApp";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import "./styles.scss";
 
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <JournalApp />
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+    <Provider store={ store }>
+      <BrowserRouter>
+        <JournalApp />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
